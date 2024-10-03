30fde31fd1fe9f1b243bb9d55cf8550aca3583ef
Wai Thura Tun <maxw52493@gmail.com>
2024-10-03 23:01:12 +0700

    Initial Commit

---
+++ RickandMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickandMorty
+//
+//  Created by Wai Thura Tun on 10/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
